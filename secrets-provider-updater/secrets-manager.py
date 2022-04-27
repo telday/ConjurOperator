fc05e7fd1d738f2e65dd0eaaa6b37b0c81c97b6a
telday <ellis.wright@cyberark.com>
2022-04-27 14:30:15 -0400

    Moved secrets provider manifests to directory

--- secrets-provider-updater/secrets-manager.py
+++ secrets-provider-updater/secrets-manager.py
@@ -2,11 +2,13 @@
 import tempfile
 import os
 import subprocess
+import functools
 
 import kubernetes
 import yaml
 import kopf
 import requests
+
 from conjur_api.models import *
 from conjur_api import *
 from conjur_api.providers.simple_credentials_provider import SimpleCredentialsProvider
@@ -25,24 +27,37 @@ def get_conjur_client():
 
     return Client(connection_info, credentials_provider=provider, ssl_verification_mode=SslVerificationMode.INSECURE, async_mode=False)
 
+@functools.lru_cache
+def get_k8s_client():
+    k8s_config = kubernetes.config.load_kube_config()
+    return kubernetes.client.ApiClient()
+
+@kopf.on.startup()
+def prep_namespace(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+@kopf.on.cleanup()
+def cleanup_namespace(*args, **kwargs):
+    print('cleanup')
+    print(args, kwargs)
+
 @kopf.on.create('secrets')
 def create_fn(body, **kwargs):
     secret_name = kwargs['meta']['name']
 
-    k8s_config = kubernetes.config.load_kube_config()
-    k8s_client = kubernetes.client.ApiClient()
+    k8s_client = get_k8s_client()
 
     # Ensure that we have a conjur managed secret before running secrets provider
     if 'conjur-map' in body['data']:
-        kubernetes.utils.create_from_yaml(k8s_client, "secrets-provider-k8s.yaml", verbose=False)
+        kubernetes.utils.create_from_yaml(k8s_client, "secrets-provider-job.yaml", verbose=False)
 
 @kopf.on.field('jobs', field='spec.completions')
 def cleanup_job(body, **kwargs):
     job_name = body['metadata']['name']
     # TODO should check to make sure job ran successfully here
     if job_name == "secrets-provider":
-        k8s_config = kubernetes.config.load_kube_config()
-        k8s_client = kubernetes.client.ApiClient()
+        k8s_client = get_k8s_client()
         batch_api = kubernetes.client.BatchV1Api(k8s_client)
 
         res = batch_api.delete_namespaced_job(
